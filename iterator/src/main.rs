ea712ea8f629fda57631abf73762c2fbcbd83d1a
Nanle <nathanielnanle100@gmail.com>
2025-02-07 13:03:50 +0100

    iterators

--- iterator/src/main.rs
+++ iterator/src/main.rs
@@ -3,7 +3,7 @@
 fn main(){
 
 
-
+// .iter()
     let v1 = vec![1, 2, 3];
 
     let v1_iter = v1.iter();
@@ -12,4 +12,26 @@ fn main(){
         println!("Got: {val}");
     }
 
-}
+    /*
+    Types of iterators
+    1. .iter() : 
+    it borrows each element in the collection with out ownership.
+    Use Case: when you want to read or inspect elements without taking ownership or modifying the collection.
+     
+     2. .iter_mut()
+     borrows element of a collection and modify them(mutable)
+
+     3. into_iter()
+     consumes collections for ownership
+
+     */
+
+     let v1: Vec<i32> = vec![1, 2, 3];
+
+     let v2: Vec<_> = v1.iter().map(|x| x + 1).collect();
+ 
+     assert_eq!(v2, vec![2, 3, 4]);
+ 
+     
+     
+     }
